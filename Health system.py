08afa0e6883c46b81e3f712c6cd53c98af7101be
Mr. Wiebe <kyle.wiebe@fresnounified.org>
2016-05-17 10:21:22 -0700

    Added GUI pt 1

--- Health system.py
+++ Health system.py
@@ -25,3 +25,28 @@ orc2 = Orc()
 def kill():
     for i in range(10):
         orc1.attack(orc2)
+        
+import Tkinter
+
+root = Tkinter.Tk()
+
+scrollbar = Tkinter.Scrollbar(root)
+scrollbar.pack(side=Tkinter.RIGHT, fill = Tkinter.Y)
+
+listbox = Tkinter.Listbox(root)
+listbox.pack()
+
+button = Tkinter.Button(root, text='Quit', width=10, height = 5, command = root.destroy)
+button2 = Tkinter.Button(root, text='New Window', width=10, height = 5, command = root.destroy)
+button.pack(side = Tkinter.LEFT)
+button2.pack(side = Tkinter.LEFT)
+
+for i in range(100):
+    listbox.insert(Tkinter.END, i)
+
+# attach listbox to scrollbar
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
+
+
+Tkinter.mainloop()
